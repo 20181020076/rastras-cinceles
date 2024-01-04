1843a95576e1c68977f66b30ae25e9e370d593e8
DIEGO ALEJANDRO ROJAS REINA <diegoarojas@hotmail.es>
2024-01-04 16:07:05 -0500

    app funcionando

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,252 @@
-import Image from 'next/image'
+"use client";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+  const [mood, setMood] = useState(0);
+  // const [anchoImplento, setAnchoImplento] = useState("");
+  const [numeroDiscos, setNumeroDiscos] = useState("");
+  const [diametroDiscos, setDiametroDiscos] = useState("");
+  const [separacionCinceles, setSeparacionCinceles] = useState("");
+  const [separacionDiscos, setSeparacionDiscos] = useState("");
+  const [secciones, setSecciones] = useState(1);
+  const [penetracionDisco, setPenetracionDisco] = useState("");
+  const [profundidadPreparacion, setProfundidadPreparacion] = useState("");
+  const [numeroCinceles, setNumeroCinceles] = useState("");
+  const [profundidadCinceles, setProfundidadCinceles] = useState("");
+  const [velocidad, setVelocidad] = useState("");
+  const [coefSuelo, setCoefSuelo] = useState("");
+  const [potenciaVolante, setPotenciaVolante] = useState(0);
+  const [potenciaBt, setPotenciaBt] = useState(0);
+  const [fuerza, setFuerza] = useState(
+    ((numeroDiscos * separacionDiscos * 2.54) / secciones) *
+      coefSuelo *
+      diametroDiscos *
+      2.54 *
+      penetracionDisco
+  );
+
+  const calculoPotencia = () => {
+    if (mood == 0) {
+      const nuevaFuerza =
+        ((numeroDiscos * separacionDiscos * 2.54) / secciones) *
+        coefSuelo *
+        diametroDiscos *
+        2.54 *
+        penetracionDisco;
+
+      setFuerza(nuevaFuerza.toFixed(2));
+
+      const nuevaPotenciaBt =
+        (nuevaFuerza * ((velocidad * 1000) / 3600).toFixed(2)) / 76.04;
+      setPotenciaBt(nuevaPotenciaBt);
+
+      const nuevaPotenciaVolante = nuevaPotenciaBt / 0.7;
+      setPotenciaVolante(nuevaPotenciaVolante);
+    } else if (mood == 1) {
+      const nuevaFuerza = ((numeroCinceles * separacionCinceles) / secciones)*coefSuelo*profundidadCinceles;
+
+      setFuerza(nuevaFuerza.toFixed(2));
+     
+
+      const nuevaPotenciaBt =
+        (nuevaFuerza * ((velocidad * 1000) / 3600).toFixed(2)) / 76.04;
+      setPotenciaBt(nuevaPotenciaBt);
+
+      const nuevaPotenciaVolante = nuevaPotenciaBt / 0.7;
+      setPotenciaVolante(nuevaPotenciaVolante);
+    } else {
+      console.log("no existe esa opcion");
+    }
+  };
+  useEffect(() => {}, [mood]);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className="flex min-h-screen flex-col items-center justify-center bg-primary">
+      <div className="flex flex-col bg-secundary min-h-[90%] w-[90%] rounded-xl p-4 justify-start items-center">
+        <h1 className="font-bold text-xl mb-3">Calculo de potencias</h1>
+        <div className="flex">
+          <div
+            className={`rounded-2xl p-1 border border-white ${
+              mood == 0 ? "bg-primary text-white" : "bg-secundary"
+            }`}
+            onClick={() => setMood(0)}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+            Rastras
+          </div>
+          <div
+            className={`rounded-2xl p-1 border border-white ${
+              mood == 1 ? "bg-primary text-white" : "bg-secundary"
+            }`}
+            onClick={() => setMood(1)}
+          >
+            Cinceles
+          </div>
+        </div>
+        {mood == 0 ? (
+          <>
+            {/* Rastras */}
+            <label htmlFor="numeroDiscos">Numero de Discos</label>
+            <input
+              type="number"
+              id="numeroDiscos"
+              name="numeroDiscos"
+              value={numeroDiscos}
+              onChange={(e) => setNumeroDiscos(e.target.value)}
             />
-          </a>
+            <label htmlFor="diametroDiscos">Diametro del disco (pul)</label>
+            <input
+              type="number"
+              id="diametroDiscos"
+              name="diametroDiscos"
+              value={diametroDiscos}
+              onChange={(e) => setDiametroDiscos(e.target.value)}
+            />
+            <label htmlFor="separacionDiscos">
+              Separacion entre Discos (pulg)
+            </label>
+            <input
+              type="number"
+              id="separacionDiscos"
+              name="separacionDiscos"
+              value={separacionDiscos}
+              onChange={(e) => setSeparacionDiscos(e.target.value)}
+            />
+            <label htmlFor="velocidad">Velocidad (km/hr)</label>
+            <input
+              type="number"
+              id="velocidad"
+              name="velocidad"
+              value={velocidad}
+              onChange={(e) => setVelocidad(e.target.value)}
+            />
+            <label htmlFor="secciones">Secciones</label>
+            <input
+              type="number"
+              id="secciones"
+              name="secciones"
+              value={secciones}
+              onChange={(e) => setSecciones(e.target.value)}
+            />
+            <label htmlFor="penetracionDisco">Penetracion del disco</label>
+            <input
+              type="number"
+              id="penetracionDisco"
+              name="penetracionDisco"
+              value={penetracionDisco}
+              onChange={(e) => setPenetracionDisco(e.target.value)}
+            />
+            <label htmlFor="coefSuelo">
+              Coeficiente, Rotura suelo (Kg/Cm2)
+            </label>
+            <input
+              type="number"
+              id="coefSuelo"
+              name="coefSuelo"
+              value={coefSuelo}
+              onChange={(e) => setCoefSuelo(e.target.value)}
+            />
+            <p>Profundidad de preparacion (cm)</p>
+            <div>{diametroDiscos * 2.54 * penetracionDisco}</div>
+
+            <label htmlFor="ancho">Ancho total del implemento (cm)</label>
+            <div>{(numeroDiscos * separacionDiscos * 2.54) / secciones}</div>
+          </>
+        ) : (
+          <>
+            {/* Cinceles */}
+            <label htmlFor="numeroCinceles">Numero de Cinceles</label>
+
+            <input
+              type="number"
+              id="numeroCinceles"
+              name="numeroCinceles"
+              value={numeroCinceles}
+              onChange={(e) => setNumeroCinceles(e.target.value)}
+            />
+            <label htmlFor="profundidadCinceles">
+              Profundidad del Cincele (cm)
+            </label>
+            <input
+              type="number"
+              id="profundidadCinceles"
+              name="profundidadCinceles"
+              value={profundidadCinceles}
+              onChange={(e) => setProfundidadCinceles(e.target.value)}
+            />
+            <label htmlFor="separacionCinceles">
+              Separacion entre Cinceles (cm)
+            </label>
+            <input
+              type="number"
+              id="separacionCinceles"
+              name="separacionCinceles"
+              value={separacionCinceles}
+              onChange={(e) => setSeparacionCinceles(e.target.value)}
+            />
+            <label htmlFor="velocidad">Velocidad (Km/hr)</label>
+            <input
+              type="number"
+              id="velocidad"
+              name="velocidad"
+              value={velocidad}
+              onChange={(e) => setVelocidad(e.target.value)}
+            />
+            <label htmlFor="secciones">Secciones</label>
+            <input
+              type="number"
+              id="secciones"
+              name="secciones"
+              value={secciones}
+              onChange={(e) => setSecciones(e.target.value)}
+            />
+            <label htmlFor="coefSuelo">
+              Coeficiente, Rotura suelo (Kg/Cm2)
+            </label>
+            <input
+              type="number"
+              id="coefSuelo"
+              name="coefSuelo"
+              value={coefSuelo}
+              onChange={(e) => setCoefSuelo(e.target.value)}
+            />
+            <p>Ancho total del implemento (cm)</p>
+            <div>{(numeroCinceles * separacionCinceles) / secciones}</div>
+          </>
+        )}
+
+        {/* <select
+          id="coefSuelo"
+          name="coefSuelo"
+          value={coefSuelo}
+          onChange={(e) => setCoefSuelo(e.target.value)}
+        >
+          <option value="opcion1">Muy blandas 0,25</option>
+          <option value="opcion2">Blandas 0,25-0,50</option>
+          <option value="opcion3">Media 0,50-1,00</option>
+          <option value="opcion4">Firme 1,00-2,00</option>
+          <option value="opcion5">Muy Firme 2,00-4,00</option>
+          <option value="opcion3">Dura +4,00</option>
+        </select> */}
+        <button
+          onClick={() => calculoPotencia()}
+          className="border text-white bg-primary p-1 rounded-full"
+        >
+          Calcular
+        </button>
+        <div className="flex flex-col">
+          <div>
+            <span className="font-bold">Potencia Voltante:</span>
+            <>{potenciaVolante.toFixed(2)}</>{" "}
+          </div>
+          <div>
+            <span className="font-bold">Potencia Bt en (hp): </span>
+            {potenciaBt.toFixed(2)}
+          </div>
+          <div>
+            <span className="font-bold">Fuerza en (kg-f): </span>
+            {fuerza}
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,11 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        primary: "#367C2B", // 240,86,199
+        secundary: "#FFD300",
+
+      },
     },
   },
   plugins: [],
